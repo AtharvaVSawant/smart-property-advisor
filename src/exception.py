aec11de908fc1e571f13410782ac8b3c4960e567
AtharvaVSawant <atharvasawant3183@gmail.com>
2026-04-16 20:29:55 +0530

    Added notebooks and dataset

--- src/exception.py
+++ src/exception.py
@@ -18,10 +18,4 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-    if __name__ =="__main__":
-        try:
-            a = 1/0
-        except:
-            logging.info("Divide by 0")
-            raise CustomException(e,sys)
-        
+
