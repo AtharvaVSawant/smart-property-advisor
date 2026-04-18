1ae6e2bbb1bde425a78212401b7973982395c752
AtharvaVSawant <atharvasawant3183@gmail.com>
2026-04-19 00:29:33 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass   # use to creatclass variable 
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 @dataclass   # Decorator  why - because inside a class to define the class variable we use init but by using this we can directly define the data classes 
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -20,7 +24,7 @@ class DataIngestion:
     def intiate_data_ingestion(self):
         logging.info("Entered the data ingestion or component")
         try:
-            df = pd.read_csv(r'notebook\data\boston_clean.csv')
+            df = pd.read_csv(r'notebook\data\boston.csv')
             logging.info("Read the dataset as dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -39,13 +43,14 @@ class DataIngestion:
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path,
 
-
             )
 
         except Exception as e:
             raise CustomException(e,sys)
         
-if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.intiate_data_ingestion()
-
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data = obj.intiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
