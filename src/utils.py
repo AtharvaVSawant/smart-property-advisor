72fb63fde3de5041bb8e28685a148ce8366b5461
AtharvaVSawant <atharvasawant3183@gmail.com>
2026-05-02 00:41:39 +0530

    utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,27 +1,18 @@
 import os
 import sys
-
+import pickle
 import numpy as np
 import pandas as pd
 
-from src.exception import CustomException
-
-import os
-import sys
-
-import numpy as np 
-import pandas as pd
-import dill
-import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
@@ -30,37 +21,42 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
+        for model_name, model in models.items():
 
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
+            para = param.get(model_name, {})
 
+            # GridSearch
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            # Best model
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
+            model.fit(X_train, y_train)
 
-            #model.fit(X_train, y_train)  # Train model
-
+            # Predictions
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
-            train_model_score = r2_score(y_train, y_train_pred)
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
 
-            test_model_score = r2_score(y_test, y_test_pred)
-
-            report[list(models.keys())[i]] = test_model_score
+            # ✅ CRITICAL FIX → return trained model also
+            report[model_name] = {
+                "model": model,
+                "score": test_score
+            }
 
         return report
 
     except Exception as e:
         raise CustomException(e, sys)
-    
+
+
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
